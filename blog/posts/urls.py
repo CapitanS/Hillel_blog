694d74172e442d5c9aa2a95ef76b955183d47f4f
Vitalii-Bil <bilienko223@gmail.com>
2021-02-04 19:46:02 +0200

    PostCreate

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/create/', views.PostCreate.as_view(), name='post_create'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+
+from .models import Post, Comment
 
 
 # Create your views here.
@@ -6,3 +11,10 @@ def index(request):
     """View function for home page of site."""
 
     return render(request, 'index.html')
+
+
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'short_description', 'image', 'full_description', 'posted']
+    template_name = 'posts/post_create_page.html'
+    #success_url = reverse_lazy('posts:post_list')
