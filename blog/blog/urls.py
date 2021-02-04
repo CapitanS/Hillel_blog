934824732d90fe3a1d4b50db9d4a68856e31a20a
dimat <dimaprizma2016@gmail.com>
2021-02-04 19:22:08 +0200

    - added accounts login, logout etc

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='blog/log.html')),
+]
+
+# Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
