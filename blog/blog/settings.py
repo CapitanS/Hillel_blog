04365129fdfa449046ec7a4166b8c55ad868388e
dimat <dimaprizma2016@gmail.com>
2021-02-04 19:44:19 +0200

    - added static

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -119,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
