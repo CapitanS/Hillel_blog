0c5b414e7b3f0814f5831a693d04cee7bcc22943
Vitalii-Bil <bilienko223@gmail.com>
2021-02-08 16:55:44 +0200

    fixtures

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from .forms import CommentForm
 from .models import Comment, Post
