28a6f806495219879da6986803820fd21c1d3ace
dimat <dimaprizma2016@gmail.com>
2021-02-08 15:25:24 +0200

    - added schema - added field moderated for Comment in models.py

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
 
+# Create your models here.
 class Post(models.Model):
     title = models.CharField(_("title"), max_length=100)
     short_description = models.CharField(_("short description"), max_length=200)
-    image = models.URLField(max_length = 400)
+    image = models.URLField(max_length=400)
     full_description = models.TextField(_("full description"), blank = True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     posted = models.BooleanField(_('posted'), default=False)
@@ -19,7 +19,8 @@ class Post(models.Model):
 class Comment(models.Model):
     username = models.CharField(_("username"), max_length=100)
     text = models.CharField(_("text"), max_length=300)
-    post = models.ForeignKey("Post", on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    moderated = models.BooleanField(_('moderated'), default=False)
 
     def __str__(self):
         return self.username
