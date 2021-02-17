70c6c76a75e552f72c7b2141ad5ff2b46b99d2f8
Vitalii-Bil <bilienko223@gmail.com>
2021-02-17 16:55:29 +0200

    -rem different settings

--- blog/settings/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-from __future__ import absolute_import
-
-# When defaults are loaded assume a local development environment
-# All other environments should explicitly choose a right settings
-from .production import *  # noqa
