5b4ab3ec4c35530c48e06f21eb24282db5001a18
Vitalii-Bil <bilienko223@gmail.com>
2021-02-17 15:23:18 +0200

    flake8 fixes

--- blog/settings/__init__.py
+++ blog/settings/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 
 # When defaults are loaded assume a local development environment
 # All other environments should explicitly choose a right settings
-from .local import *
+from .local import *  # noqa
