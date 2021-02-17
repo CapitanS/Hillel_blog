3df69e07110b7b641124f4ca26a842c18b726f89
Vitalii-Bil <bilienko223@gmail.com>
2021-02-17 15:38:39 +0200

    -set settings to prod

--- blog/settings/__init__.py
+++ blog/settings/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 
 # When defaults are loaded assume a local development environment
 # All other environments should explicitly choose a right settings
-from .local import *  # noqa
+from .production import *  # noqa
